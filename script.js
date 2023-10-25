a576df0001981fba87528ee4f823721b7d3951ed
omarnagy91 <omargnagy@hotmail.com>
2023-10-25 20:20:51 +0300

    styling the table

--- script.js
+++ script.js
@@ -1,160 +1,183 @@
 /* script.js */
-function syncButtonClicked(buttonElement) {
-    // Get button and URN
-    var button = jQuery(buttonElement);
-    var urn = button.data("urn");
+jQuery(document).ready(function ($) {
 
-    // Update button text
-    button.text("...");
+    function syncButtonClicked(buttonElement) {
+        // Get button and URN
+        var button = jQuery(buttonElement);
+        var urn = button.data("urn");
 
-    // Make AJAX request
-    jQuery('.delete-button').click(function () {
-        var postId = jQuery(this).data('id');
+        // Update button text
+        button.text("...");
 
+        // Make AJAX request
+        jQuery('.delete-button').click(function () {
+            var postId = jQuery(this).data('id');
+
+            jQuery.ajax({
+                url: ajaxurl,
+                type: 'POST',
+                data: {
+                    action: 'delete_post',
+                    id: postId
+                },
+                success: function (response) {
+                    // Remove the post from the table
+                    jQuery('#post-' + postId).remove();
+                }
+            });
+        });
+
+        jQuery('#posts-table tbody').sortable({
+            update: function (event, ui) {
+                var postOrder = jQuery(this).sortable('toArray');
+
+                jQuery.ajax({
+                    url: ajaxurl,
+                    type: 'POST',
+                    data: {
+                        action: 'update_post_order',
+                        post_order: postOrder
+                    }
+                });
+            }
+        });
+        jQuery.ajax({
+            url: "https://scrape-js.onrender.com/",
+            type: "POST",
+            data: JSON.stringify({
+                urn: urn,
+                secretKey: "PzoiJcU2ocfOeWj6AQQdkQ"
+            }),
+            contentType: "application/json",
+            success: function (response) {
+
+                // Update button text
+                button.text("Sync");
+
+                // Get data
+                var data = response;
+
+                // Make update AJAX request
+                jQuery.ajax({
+                    url: ajaxurl,
+                    type: "POST",
+                    data: {
+                        action: "update_row",
+                        data: JSON.stringify({
+                            urn: data.urn,
+                            author: data.author,
+                            username: data.username,
+                            age: data.age,
+                            profilePicture: data.profilePicture,
+                            post_text: data.post_text,
+                            images: data.images,
+                            reactions: data.reactions,
+                            comments: data.comments,
+                            synced: true
+                        })
+                    },
+                    success: function (response) {
+
+                        // Update button text
+                        button.text("Sync");
+
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+
+                        // Update button text
+                        button.text("Sync");
+
+                    }
+                });
+
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+
+                // Update button text
+                button.text("Sync");
+
+            }
+        });
+    }
+
+    function publishButtonClicked(buttonElement) {
+        // Get button and ID
+        var button = jQuery(buttonElement);
+        var id = button.data("id");
+        var published = button.data("published");
+
+        // Update button text  
+        button.text('...').addClass('loading');
+
+        // Make publish/unpublish AJAX request
         jQuery.ajax({
             url: ajaxurl,
-            type: 'POST',
+            type: "POST",
             data: {
-                action: 'delete_post',
-                id: postId
+                action: "publish_unpublish",
+                id: id,
+                published: !published
             },
             success: function (response) {
-                // Remove the post from the table
-                jQuery('#post-' + postId).remove();
+                // Update button text and data
+                button.text(published ? 'Publish' : 'Unpublish').removeClass('loading');
+                button.data("published", !published);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                // Log error
+                console.log("Error: " + textStatus + ", " + errorThrown);
+
+                // Reset button text
+                button.text(published ? 'Unpublish' : 'Publish').removeClass('loading');
+            }
+        });
+    }
+
+    $('form input[type=submit]').on('click', function (e) {
+        e.preventDefault();
+        const form = $(this).closest('form');
+        const button = $(this);
+        button.val('...').addClass('loading');
+        $.ajax({
+            url: ajaxurl,
+            type: 'POST',
+            data: form.serialize(),
+            success: function (response) {
+                button.val('Delete').removeClass('loading');
+                form.closest('tr').remove();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                button.val('Delete').removeClass('loading');
+                console.error('Error:', textStatus, errorThrown);
             }
         });
     });
 
-    jQuery('#posts-table tbody').sortable({
-        update: function (event, ui) {
-            var postOrder = jQuery(this).sortable('toArray');
 
-            jQuery.ajax({
-                url: ajaxurl,
-                type: 'POST',
-                data: {
-                    action: 'update_post_order',
-                    post_order: postOrder
-                }
-            });
-        }
-    });
-    jQuery.ajax({
-        url: "https://scrape-js.onrender.com/",
-        type: "POST",
-        data: JSON.stringify({
-            urn: urn,
-            secretKey: "PzoiJcU2ocfOeWj6AQQdkQ"
-        }),
-        contentType: "application/json",
-        success: function (response) {
+    jQuery(document).ready(function ($) {
+        $('#posts-table tbody').sortable({
+            update: function (event, ui) {
+                var postOrder = jQuery(this).sortable('toArray');
 
-            // Update button text
-            button.text("Sync");
-
-            // Get data
-            var data = response;
-
-            // Make update AJAX request
-            jQuery.ajax({
-                url: ajaxurl,
-                type: "POST",
-                data: {
-                    action: "update_row",
-                    data: JSON.stringify({
-                        urn: data.urn,
-                        author: data.author,
-                        username: data.username,
-                        age: data.age,
-                        profilePicture: data.profilePicture,
-                        post_text: data.post_text,
-                        images: data.images,
-                        reactions: data.reactions,
-                        comments: data.comments,
-                        synced: true
-                    })
-                },
-                success: function (response) {
-
-                    // Update button text
-                    button.text("Sync");
-
-                },
-                error: function (jqXHR, textStatus, errorThrown) {
-
-                    // Update button text
-                    button.text("Sync");
-
-                }
-            });
-
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-
-            // Update button text
-            button.text("Sync");
-
-        }
-    });
-}
-
-function publishButtonClicked(buttonElement) {
-    // Get button and ID
-    var button = jQuery(buttonElement);
-    var id = button.data("id");
-    var published = button.data("published");
-
-    // Update button text  
-    button.text("...");
-
-    // Make publish/unpublish AJAX request
-    jQuery.ajax({
-        url: ajaxurl,
-        type: "POST",
-        data: {
-            action: "publish_unpublish",
-            id: id,
-            published: !published
-        },
-        success: function (response) {
-
-            // Update button text and data
-            button.text(published ? "Publish" : "Unpublish");
-            button.data("published", !published);
-
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-
-            // Log error
-            console.log("Error: " + textStatus + ", " + errorThrown);
-
-            // Reset button text
-            button.text(published ? "Unpublish" : "Publish");
-
-        }
+                jQuery.ajax({
+                    url: ajaxurl,
+                    type: 'POST',
+                    data: {
+                        action: 'update_post_order',
+                        post_order: postOrder
+                    },
+                    success: function (response) {
+                        // Handle success
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        // Handle error
+                    }
+                });
+            }
+        });
     });
 
-}
 
-jQuery(document).ready(function ($) {
-    $('#posts-table tbody').sortable({
-        update: function (event, ui) {
-            var postOrder = jQuery(this).sortable('toArray');
-
-            jQuery.ajax({
-                url: ajaxurl,
-                type: 'POST',
-                data: {
-                    action: 'update_post_order',
-                    post_order: postOrder
-                },
-                success: function (response) {
-                    // Handle success
-                },
-                error: function (jqXHR, textStatus, errorThrown) {
-                    // Handle error
-                }
-            });
-        }
-    });
 });
+
+
