c0f7bed9970fb0ddb7c3040b1687a759de18317a
omarnagy91 <omargnagy@hotmail.com>
2023-11-10 22:26:20 +0200

    .

--- linkedin-posts-slider.php
+++ linkedin-posts-slider.php
@@ -26,7 +26,6 @@ require_once plugin_dir_path(__FILE__) . 'src/widget-registration.php';
 require_once plugin_dir_path(__FILE__) . 'src/options-page.php';
 require_once plugin_dir_path(__FILE__) . 'src/scrapper-options-page.php';
 require_once plugin_dir_path(__FILE__) . 'src/db-table-creation.php';
-require_once plugin_dir_path(__FILE__) . 'src/cron-event.php';
 require_once plugin_dir_path(__FILE__) . 'src/admin-menu.php';
 require_once plugin_dir_path(__FILE__) . 'src/ajax-actions.php';
 //require_once plugin_dir_path(__FILE__) . 'src/linkedin-posts-syncing.php';
